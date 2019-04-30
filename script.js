86f9366550d1dcdc85b30b00ab97a2d4809449b7
Heather Wenzel <hcwenzel@gmail.com>
2019-04-29 21:02:38 -0400

    First commit for JS lab 1 pt 1

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let userHealth = 40;
+let grantHealth = 10;
+let userDamage = 0;
+let grantDamage = 0;
+let userWins = 0;
+let grantWins = 0;
+
+let play = prompt("Do you want to play a game?", "Yes or No");
+if (play === "Yes") {
+    let user = prompt("Please enter your name.");
+    while (userWins < 3 && grantWins < 1) {
+        if (userHealth > 0 && grantHealth > 0) {
+            userDamage = Math.floor((Math.random() * 2) + 1);
+            grantDamage = Math.floor((Math.random() * 2) + 1);
+            userHealth -= userDamage;
+            console.log(`${user} has ${userHealth} health and ${userWins} wins.`);
+            grantHealth -= grantDamage;
+            console.log(`The Almighty Grant has ${grantHealth} health and ${grantWins} wins.`);
+            if (grantHealth <= 0) {
+                console.log(`${user} beat The Almighty Grant!`)
+                userWins++;
+                grantHealth = 10;
+            } if (userWins === 3) {
+                console.log(`${user} wins!`)
+                break;
+            } if (userHealth <= 0) {
+                console.log(`${user} has died. The Almighty Grant wins.`)
+                grantWins++;
+                break;
+            }
+        }
+    }
+}
